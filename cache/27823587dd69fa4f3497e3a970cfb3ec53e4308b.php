83411e0c8f383ebeae4ae7b36217612c63cb307b
Hem Bahadur Dangaura <mayachy55@gmail.com>
2026-01-21 07:55:53 +0545

    again

---
+++ cache/27823587dd69fa4f3497e3a970cfb3ec53e4308b.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+</head>
+<body>
+
+<h2>Simple Employee CRUD</h2>
+<hr>
+
+<?php echo $__env->yieldContent('content'); ?>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\FSD_week8\app\views/layout.blade.php ENDPATH**/ ?>
